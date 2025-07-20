66b94e81743d65bd63c5091eaa3ddb4c57e8db06
Adam Kruchkow <GreenSpiny@users.noreply.github.com>
2025-07-19 20:01:50 -0400

    Begin card postprocessing

--- Data Workspace/VG_CardUrls/PostprocessCardInfo.py
+++ Data Workspace/VG_CardUrls/PostprocessCardInfo.py
@@ -1,10 +1,25 @@
 import json
 
 dataFile = open('allCardsSingleton.json', 'r+')
-data = json.loads(urlsFile.read())['cards']
+data = json.loads(dataFile.read())
+cards = data['cards']
 
 index = 0 # first use
-index = len(data.keys()) # later uses
-print(index)
+# index = len(cards.keys()) # later uses
 
+for c in cards.values():
+	if c['effect'] == '-':
+		c['effect'] = ''
+	if 'version' not in c.keys():
+		c['version'] = 0
+	if isinstance(c['skill'], str):
+		skillArray = c['skill'].split(',')
+		for i in range(len(skillArray)):
+			skillArray[i] = skillArray[i].strip()
+		c['skill'] = skillArray
+
+
+dataFile.seek(0)
+dataFile.truncate(0)
+dataFile.write(json.dumps(data, sort_keys=True, indent=1))
 dataFile.close()
